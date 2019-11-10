82c17f87d7c36ad4a707f46068c8b275ffdb339d
yingqi9 <joanaqyq@gmail.com>
2019-11-10 17:04:43 +0800

    add messages

--- instagram_web/blueprints/sessions/templates/views.py
+++ instagram_web/blueprints/sessions/templates/views.py
@@ -1,5 +1,5 @@
-from flask import Flask, sessions, Blueprint, render_template, request, redirect, url_for
-from models.user import *
+from flask import Flask, Blueprint, sessions, render_template, request, redirect, url_for, flash, abort
+from models.user import User
 from werkzeug.security import check_password_hash
 
 sessions_blueprint = Blueprint('sessions', __name__, template_folder='templates')
@@ -8,3 +8,12 @@ sessions_blueprint = Blueprint('sessions', __name__, template_folder='templates'
 @sessions_blueprint.route('/new', methods=['GET'])
 def new():
     return render_template("sessions/new.html")
+
+@sessions_blueprint.route('/', methods=['POST'])
+def create():
+    username = request.form.get('username')
+    password_to_check = request.form['password']
+
+
+
+
